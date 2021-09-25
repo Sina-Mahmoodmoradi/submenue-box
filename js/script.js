05f3493cfedd0650bad2d87a0871cb1ddaf77cb3
Sina-Mahmoodmoradi <sina.mahmudmoradi@gmail.com>
2021-09-25 20:26:05 +0330

    second

--- js/script.js
+++ js/script.js
@@ -1,1 +1,6 @@
-
+let button = document.getElementById('buttonImg');
+button.addEventListener('click', changeId);
+let classList = document.getElementById('container').classList;
+function changeId() {
+    classList.toggle('show');
+}
